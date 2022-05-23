aa32bef189efffb19c1d2083d09f5d02449c3eb1
john.njau <john.njoroge@student.moringaschool.com>
2022-05-23 14:04:28 +0300

    feature: Adding templates

--- news/views.py
+++ news/views.py
@@ -1,9 +1,10 @@
 import datetime as dt
+from django.shortcuts import render
 from django.http import HttpResponse, Http404
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse("Welcome to the Moringa Tribune")
+    return render(request, 'welcome.html')
 
 def news_of_day(request):
     date = dt.date.today()
