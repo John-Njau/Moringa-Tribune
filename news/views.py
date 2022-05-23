b0841045631ba1f74a3d17be9e831759e83e8155
john.njau <john.njoroge@student.moringaschool.com>
2022-05-23 13:41:45 +0300

    past news view function

--- news/views.py
+++ news/views.py
@@ -1,6 +1,46 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import datetime as dt
+from django.http import HttpResponse, Http404
 
 # Create your views here.
 def welcome(request):
     return HttpResponse("Welcome to the Moringa Tribune")
+
+def news_of_day(request):
+    date = dt.date.today()
+    
+    # Function to convert date object to find exact day
+    day = convert_dates(date)
+    html = f'''
+    <html>
+        <body>
+            <h1>News for {day} </h1> <em> {date.day}-{date.month}-{date.year} </em>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
+
+def convert_dates(dates):
+    #function that gets the weekday number for the date 
+    day_number = dt.date.weekday(dates)
+    
+    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    
+    #returning the actualday of the week
+    day = days[day_number]
+    return day
+
+def past_days_news(request, past_date):
+    try:
+    #converts data from the string url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+    except ValueError:
+        #raise 404 error
+        raise Http404()
+    
+    day = convert_dates(date)
+    html = f'''
+    <html>
+    <body>News for {day} {date.day}-{date.month}-{date.year}</body>
+    </html>
+    '''
+    return HttpResponse(html)
--- tribune/urls.py
+++ tribune/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# re_path -- regex exp
 from django.contrib import admin
-from django.urls import path
+from django.urls import re_path as url, include
+from django.urls import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('^admin/', admin.site.urls),
+    url('^news/', include('news.urls'))
 ]
