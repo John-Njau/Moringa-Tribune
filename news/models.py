8622babc265cc99b64048c813eda6a52475e9716
john.njau <john.njoroge@student.moringaschool.com>
2022-05-24 07:22:45 +0300

    feature: creating models and configuring database

--- news/models.py
+++ news/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Editor(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    
+    def __str__(self):
+        return self.first_name
+    class Meta:
+        ordering = ('first_name',)
+    
+class tags(models.Model):
+    name  = models.CharField(max_length=30)
+    def __str__(self):
+        return self.name
+    
+# one to many relationship
+class Article(models.Model):
+    title = models.CharField(max_length=60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(tags)
+    
+    def __str__(self):
+        return self.title
+    
+# many to many relationships
